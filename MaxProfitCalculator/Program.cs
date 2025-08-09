3a1ebb5a4b842129280d5b31b582864c86f34e27
goga0301 <54044928+goga0301@users.noreply.github.com>
2025-08-10 03:10:40 +0400

    Add unit tests for maxProfit method and refactor logic (squash merge)
    * Add unit tests for maxProfit method and refactor to MaxProfitHelper class
    
    * Remove extra UnitTest1.cs file as per PR review

--- MaxProfitCalculator/Program.cs
+++ MaxProfitCalculator/Program.cs
@@ -1,40 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
 List<int> prices = [5, 5, 2, 5, 5, 7, 9, 1, 5, 6];
 
-var profit = maxProfit(prices);
+var profit = MaxProfitHelper.maxProfit(prices);
 
 Console.WriteLine(profit);
 
-static int maxProfit(List<int> stockPrices)
+public class MaxProfitHelper
 {
-
-    
-    var maxProfit = 0;
-    var min = stockPrices[0];
-
-    for (int i = 1; i < stockPrices.Count; i++)
+    public static int maxProfit(List<int> stockPrices)
     {
-        int stockPrice = stockPrices[i];
+        var maxProfit = 0;
+        var min = stockPrices[0];
 
-        if(stockPrice - min > maxProfit)
+        for (int i = 1; i < stockPrices.Count; i++)
         {
-            maxProfit = stockPrice - min;
+            int stockPrice = stockPrices[i];
+
+            if(stockPrice - min > maxProfit)
+            {
+                maxProfit = stockPrice - min;
+            }
+
+            if(stockPrice < min)
+            {
+                min = stockPrice;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("-----------");
+            Console.WriteLine($"Price {stockPrice}");
+            Console.WriteLine($"Min {min}");
+            Console.WriteLine($"MaxProfit {maxProfit}");
         }
 
-        if(stockPrice < min)
-        {
-            min = stockPrice;
-        }
-
-        Console.WriteLine();
-        Console.WriteLine("-----------");
-        Console.WriteLine($"Price {stockPrice}");
-        Console.WriteLine($"Min {min}");
-        Console.WriteLine($"MaxProfit {maxProfit}");
-
+        return maxProfit;
     }
-
-    return maxProfit;
 }
