e530a999241bf9d0f0fcea48960d0e606809652d
goga0301 <54044928+goga0301@users.noreply.github.com>
2025-08-10 03:20:51 +0400

    Add null and empty checks to maxProfit method (#4)

--- MaxProfitCalculator/Program.cs
+++ MaxProfitCalculator/Program.cs
@@ -1,4 +1,3 @@
-﻿
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
@@ -12,6 +11,21 @@ public class MaxProfitHelper
 {
     public static int maxProfit(List<int> stockPrices)
     {
+        if (stockPrices == null)
+        {
+            throw new ArgumentNullException(nameof(stockPrices), "Stock prices list cannot be null.");
+        }
+
+        if (stockPrices.Count == 0)
+        {
+            throw new ArgumentException("Stock prices list cannot be empty.", nameof(stockPrices));
+        }
+
+        if (stockPrices.Count == 1)
+        {
+            return 0; // Can't make profit with just one price
+        }
+
         var maxProfit = 0;
         var min = stockPrices[0];
 
