f821f4b5249447cc3857ca3bca1b59a68eb105b5
Goga Gachechiladze <goga200330023@gmail.com>
2025-08-10 02:52:36 +0400

    Add .gitignore and initial project files

---
+++ MaxProfitCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+List<int> prices = [5, 5, 2, 5, 5, 7, 9, 1, 5, 6];
+
+var profit = maxProfit(prices);
+
+Console.WriteLine(profit);
+
+static int maxProfit(List<int> stockPrices)
+{
+
+    
+    var maxProfit = 0;
+    var min = stockPrices[0];
+
+    for (int i = 1; i < stockPrices.Count; i++)
+    {
+        int stockPrice = stockPrices[i];
+
+        if(stockPrice - min > maxProfit)
+        {
+            maxProfit = stockPrice - min;
+        }
+
+        if(stockPrice < min)
+        {
+            min = stockPrice;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("-----------");
+        Console.WriteLine($"Price {stockPrice}");
+        Console.WriteLine($"Min {min}");
+        Console.WriteLine($"MaxProfit {maxProfit}");
+
+    }
+
+    return maxProfit;
+}
